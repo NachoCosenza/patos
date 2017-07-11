d33bb6e81ae2b79d56878c3cb9eea4876c5696ff
NachoCosenza <cosenza.lores@hotmail.com>
2017-07-11 18:08:27 -0300

    csoenza
    proyecto shot

---
+++ patos shot/clase 7/Assets/patos/mira.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mira : MonoBehaviour 
+{
+	public Transform pato;
+	private Rigidbody rb;
+	private Vector3 lugarInicio;
+	private Quaternion QuaInucio;
+	public float poder;
+	private float contador = 0.0f;
+	public float subir;
+	private bool disparado = false;
+	void OnMuoseEnter ()
+	{
+		Debug.Log("enter");
+	}
+
+	void OnMouseDown ()
+	{
+		Debug.Log("down");
+		rb.AddRelativeForce (Vector3.down * poder);
+
+		disparado = true;
+	
+	}
+
+
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		lugarInicio = transform.position;
+		QuaInucio = transform.rotation;
+	}
+
+
+
+	void Update ()
+		{
+		if (disparado== true) 
+		{
+			contador += Time.deltaTime;
+			print ("ooooo");
+
+		}
+			
+
+		if (contador >= subir) 
+			{
+				rb.velocity = Vector3.zero;
+				rb.angularVelocity = Vector3.zero;
+
+				rb.MovePosition (lugarInicio);
+				rb.MoveRotation (QuaInucio);
+
+				contador = 0;
+			disparado = false;
+
+
+				print ("puo");
+
+			}
+
+	
+		}
+}
